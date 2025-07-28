439f08d9efcc9f25e8cb12c0befb494e14abe72a
kaladin <335095@niuitmo.ru>
2025-07-28 13:10:52 +0300

    more spell

--- sources/tolk-toolchain/compileAll.ts
+++ sources/tolk-toolchain/compileAll.ts
@@ -4,7 +4,7 @@ const main = async () => {
     const res = await compileAll()
 
     // maybe write fift to output
-    // or save bocs to filesystem later
+    // or save boc to filesystem later
     console.log(`Successfully compiled ${Object.keys(res).length} tolk contracts`)
 }
 
